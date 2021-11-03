7244f52a49ec3961c04b7fdf086821d39d256df0
Yanni App13 <jonvincruman@gmail.com>
2021-11-02 20:37:39 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>AppleBook</title>
+      </Head>
+      <Header />
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+    </div>
+
+  )
+}
